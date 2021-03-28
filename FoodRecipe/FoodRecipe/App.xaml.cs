6a9893b1962d77bcf4873c49a2a26c99a2cabae9
Wisdom Jere <wisdomjere5@gmail.com>
2021-03-29 00:23:56 +0200

    Add project files.

---
+++ FoodRecipe/FoodRecipe/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using FoodRecipe.Pages;
+
+
+namespace FoodRecipe
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage (new Foods());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ FoodRecipe/FoodRecipe/Pages/Foods.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FoodRecipe.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Foods : ContentPage
+    {
+        public Foods()
+        {
+            InitializeComponent();
+        }
+    }
+}
